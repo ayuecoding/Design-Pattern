3aeeed73e76f891f956a467b0a2ec4abbd9e7a6b
caiyue02 <caiyue02@corp.netease.com>
2021-11-25 19:22:41 +0800

    单例模式

---
+++ src/com/ayue/singleton/Singleton2.java
@@ -0,0 +1,21 @@
+package com.ayue.singleton;
+
+/**
+ * 懒汉式
+ */
+public class Singleton2 {
+    private volatile static Singleton2 instance;
+
+    private Singleton2(){}
+
+    public static Singleton2 getInstance() {
+        if (instance == null) {
+            synchronized (Singleton2.class) {
+                if (instance == null) {
+                    instance = new Singleton2();
+                }
+            }
+        }
+        return instance;
+    }
+}
